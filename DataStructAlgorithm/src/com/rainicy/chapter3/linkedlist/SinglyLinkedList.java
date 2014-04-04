a793862e5c9fcd9890556691c0e0933b662c6c28
Rainicy <rainicy925@gmail.com>
2014-04-04 19:44:56 -0400

    Add SinglyLinkedList class

--- DataStructAlgorithm/src/com/rainicy/chapter3/linkedlist/SinglyLinkedList.java
+++ DataStructAlgorithm/src/com/rainicy/chapter3/linkedlist/SinglyLinkedList.java
@@ -1,6 +1,28 @@
-
+/*
+ * SinglyLinkedList.java
+ * 
+ * Version: 1.0
+ * 
+ * Date: March 31, 2014
+ */
 package com.rainicy.chapter3.linkedlist;
 
+/**
+ * Singly linked list.
+ *
+ * @version 1.0
+ * @author Rainicy
+ * 
+ */
 public class SinglyLinkedList {
-
+	/** Head of the linked list */
+	protected Node head;
+	/** Number of nodes in the list */
+	protected long size;
+	
+	/** Default constructor that creates an empty list */
+	public SinglyLinkedList() {
+		head = null;
+		size = 0;
+	}
 }
