f2b2ac3405b5ddd1d99c489f4608a4a2d0962b0f
Rainicy <rainicy925@gmail.com>
2014-05-03 02:27:24 -0400

    Add FavoriteTest.java

---
+++ DataStructAlgorithm/src/com/rainicy/test/FavoriteTest.java
@@ -0,0 +1,61 @@
+/*
+ * FavoriteTest.java
+ * 
+ * Date: May 3, 2014
+ */
+package com.rainicy.test;
+
+import java.awt.BorderLayout;
+import java.io.IOException;
+import java.util.Random;
+
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+
+import com.rainicy.chapter6.arraylist.FavoriteList;
+import com.rainicy.chapter6.arraylist.FavoriteListMTF;
+
+/**
+ * Example for testing FavoriteList and FavoriteListMTF
+ * 
+ * @version 1.0
+ * @author Rainicy
+ */
+public class FavoriteTest {
+	public static void main(String[] args) {
+	    String[] urlArray = {"http://wiley.com", "http://datastructures.net", 
+			    "http://algorithmdesign.net", "http://www.brown.edu",
+			    "http://uci.edu" };
+	    FavoriteList<String> L1 = new FavoriteList<String>();
+	    FavoriteListMTF<String> L2 = new FavoriteListMTF<String>();
+	    int n = 20; // number of access operations
+	    // Simulation scenario: access n times a random URL
+	    Random rand = new Random();
+	    for (int k = 0; k < n; k++) {
+	      System.out.println("-------------------------------------------");
+	      int i = rand.nextInt(urlArray.length);	// random index
+	      String url = urlArray[i];			// random URL
+	      System.out.println("Accessing: " + url);
+	      L1.access(url);
+	      System.out.println("L1 = " + L1);
+	      L2.access(url);
+	      System.out.println("L2 = " + L2);
+	    }
+	    int t = L1.size()/2;
+	    System.out.println("-------------------------------------------");
+	    System.out.println("Top " + t + " in L1 = " + L1.top(t));
+	    System.out.println("Top " + t + " in L2 = " + L2.top(t));
+	    // Pop up a browser window displaying the most popular URL in L1
+	    try {
+	      String popular = L1.top(1).iterator().next(); // most popular URL in L1
+	      JEditorPane jep = new JEditorPane(popular);
+	      jep.setEditable(false);
+	      JFrame frame = new JFrame(popular);
+	      frame.getContentPane().add(new JScrollPane(jep), BorderLayout.CENTER);
+	      frame.setSize(640, 480);
+	      frame.setVisible(true);
+	    } catch (IOException e) { // ignore I/O exceptions 
+	      }
+	  }
+}
